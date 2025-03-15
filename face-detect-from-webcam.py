c29a46530a3cb36f6a8989a6f0ce36bbe3940486
voidrlm <varjun609@gmail.com>
2025-03-15 11:58:45 -0400

    Minor code change

--- face-detect-from-webcam.py
+++ face-detect-from-webcam.py
@@ -9,7 +9,7 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    cv2.imshow('img', img)
+    cv2.imshow('Face Detection', img)
     k = cv2.waitKey(30) & 0xff
     if k==27:
         break
